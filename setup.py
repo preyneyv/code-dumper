bb52a48fae2e2c09f262b23c31a08701af04f369
Pranav Nutalapati <pranavnutalapati@gmail.com>
2020-04-22 23:05:12 +0530

    Initial commit :tada:

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="code-dumper",
+    version="0.0.1",
+    author="Pranav Nutalapati",
+    author_email="pranavnutalapati@gmail.com",
+    description="A dependency analyzer and tree-shaker to dump a specific class/function.",
+    url="https://github.com/preyneyv/code-dumper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
