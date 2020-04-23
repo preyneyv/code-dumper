f83bd211a164235865e05a1bf948bbde13f613bd
Pranav Nutalapati <pranavnutalapati@gmail.com>
2020-04-23 10:52:00 +0530

    Cleaned up tests/test.py and bumped version number

--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,11 @@
-from razor.blocks import Block, inputs, outputs
-from razor.pipeline import Pipeline
 from code_dumper import pretty_print
 
 x = 4
+
+
 def mult(y):
-    return x * y
-
-@inputs.atomic.generic('asdf')
-class AddBlock(Block):
-    def run(self, asdf):
-        return mult(asdf)
+    f = lambda y: y + x
+    return f(y)
 
 
-@inputs.atomic.generic('asd')
-@outputs.atomic.generic('df')
-class AddBlock(Block):
-    def run(self, asd, df):
-        return df.put(asd)
-
-pretty_print(AddBlock, with_source=0, with_logs=1)
+pretty_print(mult, with_source=0, with_logs=1)
